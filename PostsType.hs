0026e48cea06e2768f7a22f4a25bf115debcea35
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2024-01-13 11:15:38 -0500

    Move PostsType from parent project

---
+++ PostsType.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
+module Common.PostsType
+    ( Post (..) )
+    where
+
+import GHC.Generics
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Time.Clock (UTCTime) -- Required for timestamp with time zone
+import Data.Int (Int64)
+import Data.Text (Text)
+
+data Post = Post
+    { post_id         :: Maybe Int64
+    , board_post_id   :: Int64
+    , creation_time   :: UTCTime
+    , body            :: Maybe Text
+    , name            :: Maybe Text
+    , subject         :: Maybe Text
+    , email           :: Maybe Text
+    , thread_id       :: Int
+    } deriving (Show, Generic, FromJSON, ToJSON)
