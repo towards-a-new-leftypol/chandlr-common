e0c20d51157fe8a203ed1b40eee78a2813bccee6
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2024-03-06 00:33:50 -0500

    Add collectBacklinks and quotedPosts functions from BodyParser

---
+++ Parsing/PostBodyUtils.hs
@@ -0,0 +1,29 @@
+module Common.Parsing.PostBodyUtils where
+
+import qualified Data.Map as Map
+
+import Common.Parsing.PostPartType
+import Common.Component.Thread.Model (PostWithBody)
+import Common.Parsing.QuoteLinkParser (ParsedURL (..))
+
+collectBacklinks :: [ PostWithBody ] -> Backlinks
+collectBacklinks xs = foldr insertElement Map.empty xs
+  where
+    insertElement :: PostWithBody -> Backlinks -> Backlinks
+    insertElement (post, body) acc = foldr insertPost acc (quotedPosts body)
+      where
+        insertPost postId = Map.insertWith (++) postId [post]
+
+
+quotedPosts :: [ PostPart ] -> [ Integer ]
+quotedPosts [] = []
+quotedPosts (Quote (Right (ParsedURL { postId = Just p })) : xs) = [p] ++ quotedPosts xs
+quotedPosts ((GreenText     xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
+quotedPosts ((OrangeText    xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
+quotedPosts ((RedText       xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
+quotedPosts ((Spoiler       xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
+quotedPosts ((Bold          xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
+quotedPosts ((Underlined    xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
+quotedPosts ((Italics       xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
+quotedPosts ((Strikethrough xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
+quotedPosts _ = []
