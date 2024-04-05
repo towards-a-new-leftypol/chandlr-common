202f0eb9616b6675e3fa011c69d8fda9028e5e59
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2024-04-05 01:30:23 -0400

    Expand consumer settings

--- Server/ConsumerSettings.hs
+++ Server/ConsumerSettings.hs
@@ -6,12 +6,16 @@ import Data.Aeson (FromJSON)
 data JSONSiteSettings = JSONSiteSettings
     { name :: String
     , root_url :: String
+    , boards :: [ String ]
     } deriving (Show, Generic)
 
 instance FromJSON JSONSiteSettings
 
 data ConsumerJSONSettings = ConsumerJSONSettings
-    { websites :: JSONSiteSettings
+    { websites :: [ JSONSiteSettings ]
+    , postgrest_url :: String
+    , jwt :: String
+    , media_root_path :: String
     } deriving (Show, Generic)
 
 instance FromJSON ConsumerJSONSettings
