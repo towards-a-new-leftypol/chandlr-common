d505b803bcb4c8869715d3cedee224f6fc234cb8
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2025-07-02 20:34:27 -0400

    Add key_ attribute to components
    - this doesn't have the intended result of fixing the parentNode bug
      that happens when you click a Thread

--- FrontEnd/Views.hs
+++ FrontEnd/Views.hs
@@ -16,6 +16,7 @@ import Miso
     , text
     , component_
     , onMountedWith
+    , key_
     )
 import Miso.String (toMisoString)
 import Data.Text (Text)
@@ -28,6 +29,15 @@ import qualified Common.Component.Thread as Thread
 import Common.Component.TimeControl (TimeControl)
 import Common.FrontEnd.Routes (BoardThreadId)
 
+timeControl :: TimeControl -> View Action
+timeControl tc = component_ tc [ key_ "time-control" ]
+
+grid :: Grid.GridComponent -> View Action
+grid gc = component_ gc [ key_ "grid" ]
+
+search :: View Action
+search = component_ Search.app [ key_ "search" ]
+
 catalogView :: TimeControl -> Grid.GridComponent -> Model -> View Action
 catalogView tc gc m = div_ []
     [ div_
@@ -35,9 +45,9 @@ catalogView tc gc m = div_ []
         [ h1_ [] [ text "Overboard Catalog" ]
         , time_ [] [ text $ toMisoString $ show $ current_time m ]
         ]
-    , component_ tc []
-    , component_ Search.app []
-    , component_ gc []
+    , timeControl tc
+    , search
+    , grid gc
     ]
 
 searchView :: Grid.GridComponent -> Maybe Text -> Model -> View Action
@@ -47,13 +57,17 @@ searchView gc _ m = div_ []
         [ h1_ [] [ text "Search" ]
         , time_ [] [ text $ search_term m ]
         ]
-    , component_ Search.app []
-    , component_ gc []
+    , search
+    , grid gc
     ]
 
 threadView :: Thread.ThreadComponent -> Text -> Text -> BoardThreadId -> Model -> View Action
 threadView threadComponent site_name board_pathpart board_thread_id m =
-    component_ threadComponent [ onMountedWith (const ThreadViewMounted) ]
+    component_
+        threadComponent
+        [ key_ "thread-view"
+        , onMountedWith (const ThreadViewMounted)
+        ]
 
 page404 :: View Action
 page404 = h1_ [] [ text "404 Not Found" ]
