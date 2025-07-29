756efeed61972cdca76f2e17e0e9d6581871f9fa
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2025-07-29 17:05:55 -0400

    Add Client.app to every view

--- FrontEnd/Views.hs
+++ FrontEnd/Views.hs
@@ -16,8 +16,10 @@ import Miso
     , text
     , component_
     , onMountedWith
+    , onUnmountedWith
+    , key_
     )
-import Miso.String (toMisoString)
+import Miso.String (MisoString, toMisoString)
 import Data.Text (Text)
 
 import Common.FrontEnd.Model
@@ -27,18 +29,35 @@ import qualified Common.Component.CatalogGrid as Grid
 import qualified Common.Component.Thread as Thread
 import Common.Component.TimeControl (TimeControl)
 import Common.FrontEnd.Routes (BoardThreadId)
+import qualified Network.Client as Client
 
 timeControl :: TimeControl -> View Action
 timeControl tc = component_ [] tc
 
+
 grid :: Grid.GridComponent -> View Action
 grid gc = component_ [] gc
+
 
 search :: View Action
 search = component_ [] Search.app
 
+
+pageWrapperWithDefaults :: View Action -> View Action
+pageWrapperWithDefaults inner_content =
+    div_ []
+        [ component_
+            [ onMountedWith (const ClientMounted)
+            , onUnmountedWith (const ClientUnmounted)
+            , key_ ("http-client" :: MisoString)
+            ]
+            Client.app
+        , inner_content
+        ]
+
+
 catalogView :: TimeControl -> Grid.GridComponent -> Model -> View Action
-catalogView tc gc m = div_ []
+catalogView tc gc m = pageWrapperWithDefaults $ div_ []
     [ div_
         [ class_ "page_heading" ]
         [ h1_ [] [ text "Overboard Catalog" ]
@@ -49,8 +68,9 @@ catalogView tc gc m = div_ []
     , grid gc
     ]
 
+
 searchView :: Grid.GridComponent -> Maybe Text -> Model -> View Action
-searchView gc _ m = div_ []
+searchView gc _ m = pageWrapperWithDefaults $ div_ []
     [ div_
         [ class_ "page_heading" ]
         [ h1_ [] [ text "Search" ]
@@ -60,13 +80,13 @@ searchView gc _ m = div_ []
     , grid gc
     ]
 
-threadView :: Thread.ThreadComponent -> Text -> Text -> BoardThreadId -> Model -> View Action
-threadView threadComponent site_name board_pathpart board_thread_id m = h1_ [] [ text "Hello Thread View" ]
-    {-
-    component_
+
+threadView :: Text -> Text -> BoardThreadId -> Model -> View Action
+threadView site_name board_pathpart board_thread_id m =
+    pageWrapperWithDefaults $ component_
         [onMountedWith (const ThreadViewMounted)]
-        threadComponent
-    -}
+        Thread.app
+
 
 page404 :: View Action
 page404 = h1_ [] [ text "404 Not Found" ]
