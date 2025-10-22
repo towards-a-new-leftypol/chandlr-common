64f72881f37a1e8c9b5e35edc872adf74d4befe9
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2025-10-22 17:00:29 -0400

    Move op, reply view functions into own module, make them generic
    - they still need the Thread model to render but it's easy to build that
      model.

---
+++ Component/PostViews.hs
@@ -0,0 +1,140 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Common.Component.PostViews where
+
+import Prelude hiding (head)
+import Miso
+  ( View
+  , text
+  , Attribute
+  , toMisoString
+  )
+import Miso.Html
+  ( div_
+  )
+import Miso.Html.Property
+  ( class_
+  , id_
+  )
+import Data.List.NonEmpty (head)
+import qualified Data.List as L
+
+import Common.Network.SiteType (Site)
+import qualified Common.Network.SiteType as Site
+import Common.Network.PostType (Post)
+import qualified Common.Network.PostType as Post
+import qualified Common.Network.BoardType as Board
+import Common.Network.BoardType (Board)
+import Common.Network.ThreadType (Thread)
+import Common.Component.Thread.Files (files)
+import Common.Component.Thread.Intro (intro)
+import Common.Component.Thread.Embed (embed)
+import Common.Component.Thread.Model
+import Common.Parsing.BodyParser
+import qualified Common.Component.BodyRender as Body
+import Common.Admin.DeleteBtn (deleteBtn)
+
+
+op
+    :: (PostWithBody -> action)
+    -> Model
+    -> Post
+    -> Backlinks
+    -> [ View model action ]
+op mkDeleteAction m op_post backlinks =
+    [ div_
+        (
+            [ class_ "post op"
+            , id_ $ toMisoString $ show $ Post.board_post_id op_post
+            ] ++ multi op_post
+        )
+        ( intro site_ board thread op_post backlinks (current_time m)
+        : files_or_embed_view
+        : deleteBtn_ mkDeleteAction m (L.head $ post_bodies m)
+        ++
+        [ div_
+            [ class_ "body" ]
+            (body $ post_bodies m)
+        ]
+        )
+    ]
+
+    where
+        files_or_embed_view :: View model a
+        files_or_embed_view =
+          case Post.embed op_post of
+            Just _ -> embed op_post
+            Nothing -> files (media_root m) site_ op_post
+
+
+        site_ :: Site
+        site_ = site m
+
+        board :: Board
+        board = head $ Site.boards site_
+
+        thread :: Thread
+        thread = head $ Board.threads board
+
+        body :: [ PostWithBody ] -> [ View model a ]
+        body [] = []
+        body (x:_) = Body.render site_ $ snd x
+
+
+multi :: Post -> [ Attribute a ]
+multi post
+    | length (Post.attachments post) > 1 = [ class_ "multifile" ]
+    | otherwise = []
+
+
+reply
+  :: (PostWithBody -> action)
+  -> Model
+  -> Backlinks
+  -> PostWithBody
+  -> View model action
+reply mkDeleteAction m backlinks pwb@(post, parts) = div_
+    [ class_ "postcontainer"
+    , id_ $ toMisoString $ show $ Post.board_post_id post
+    ]
+    [ div_
+        [ class_ "sidearrows" ]
+        [ text ">>" ]
+    , div_
+        ( class_ "post reply" : multi post )
+        ( intro site_ board thread post backlinks (current_time m)
+        : deleteBtn_ mkDeleteAction m pwb
+        ++
+        [ files_or_embed_view
+        , div_
+            [ class_ "body" ]
+            (Body.render site_ parts)
+        ]
+        )
+    ]
+
+    where
+        files_or_embed_view :: View model action
+        files_or_embed_view =
+          case Post.embed post of
+            Just _ -> embed post
+            Nothing -> files (media_root m) site_ post
+
+        site_ :: Site
+        site_ = site m
+
+        board :: Board
+        board = head $ Site.boards site_
+
+        thread :: Thread
+        thread = head $ Board.threads board
+
+
+deleteBtn_
+    :: (PostWithBody -> action)
+    -> Model
+    -> PostWithBody
+    -> [ View model action ]
+deleteBtn_ mkDeleteAction m p
+  | admin m = [ deleteBtn (mkDeleteAction p) ]
+  | otherwise = []
--- Component/Thread.hs
+++ Component/Thread.hs
@@ -18,7 +18,6 @@ import Miso
   ( View
   , Effect
   , text
-  , Attribute
   , Component
   , defaultEvents
   , io
@@ -37,38 +36,29 @@ import Miso.Html
   )
 import Miso.Html.Property
   ( class_
-  , id_
   )
 import qualified Miso as M
 import Miso.Binding ((-->))
 import Data.List.NonEmpty (head, toList)
-import qualified Data.List as L
 import Miso.String (toMisoString, MisoString)
 import Data.Time.Clock (getCurrentTime)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.State (modify)
 import Data.IORef (readIORef)
 
-import Common.Network.SiteType (Site)
 import qualified Common.Network.SiteType as Site
 import Common.Network.PostType (Post)
-import qualified Common.Network.PostType as Post
 import qualified Common.Network.BoardType as Board
-import Common.Network.BoardType (Board)
 import qualified Common.Network.ThreadType as Thread
-import Common.Network.ThreadType (Thread)
-import Common.Component.Thread.Files (files)
-import Common.Component.Thread.Intro (intro)
-import Common.Component.Thread.Embed (embed)
 import Common.Component.Thread.Model
 import Common.Parsing.BodyParser
 import qualified Common.Component.BodyRender as Body
 import Common.Component.Thread.Types
 import qualified Common.FrontEnd.JSONSettings as Settings
 import Common.FrontEnd.Types
-import Common.Admin.DeleteBtn (deleteBtn)
 import qualified Common.FrontEnd.Model as FE
 import qualified Common.Admin.Component.DeleteIllegalPost as DIP
+import Common.Component.PostViews (op, reply)
 
 type ThreadComponent parent = Component parent Model Action
 
@@ -146,7 +136,7 @@ view m =
     , div_
         [ class_ "thread" ]
         (  op_post thread_posts
-        ++ map (reply m backlinks) (drop 1 (post_bodies m))
+        ++ map (reply OnDeleteBtn m backlinks) (drop 1 (post_bodies m))
         )
     ]
 
@@ -162,100 +152,9 @@ view m =
 
         op_post :: [ Post ] -> [ View model Action ]
         op_post [] = [ h2_ [] [ "There's nothing here" ] ]
-        op_post (x:_) = op m x backlinks
+        op_post (x:_) = op OnDeleteBtn m x backlinks
 
         title :: MisoString
         title = toMisoString $ Site.name (site m) <> " /" <> board <> "/"
 
         board = Board.pathpart $ head $ Site.boards (site m)
-
-
-op :: Model -> Post -> Backlinks -> [ View model Action ]
-op m op_post backlinks =
-    [ div_
-        (
-            [ class_ "post op"
-            , id_ $ toMisoString $ show $ Post.board_post_id op_post
-            ] ++ multi op_post
-        )
-        ( intro site_ board thread op_post backlinks (current_time m)
-        : files_or_embed_view
-        : deleteBtn_ m (L.head $ post_bodies m)
-        ++
-        [ div_
-            [ class_ "body" ]
-            (body $ post_bodies m)
-        ]
-        )
-    ]
-
-    where
-        files_or_embed_view :: View model a
-        files_or_embed_view =
-          case Post.embed op_post of
-            Just _ -> embed op_post
-            Nothing -> files (media_root m) site_ op_post
-
-
-        site_ :: Site
-        site_ = site m
-
-        board :: Board
-        board = head $ Site.boards site_
-
-        thread :: Thread
-        thread = head $ Board.threads board
-
-        body :: [ PostWithBody ] -> [ View model a ]
-        body [] = []
-        body (x:_) = Body.render site_ $ snd x
-
-
-multi :: Post -> [ Attribute a ]
-multi post
-    | length (Post.attachments post) > 1 = [ class_ "multifile" ]
-    | otherwise = []
-
-
-reply :: Model -> Backlinks -> PostWithBody -> View model Action
-reply m backlinks pwb@(post, parts) = div_
-    [ class_ "postcontainer"
-    , id_ $ toMisoString $ show $ Post.board_post_id post
-    ]
-    [ div_
-        [ class_ "sidearrows" ]
-        [ text ">>" ]
-    , div_
-        ( class_ "post reply" : multi post )
-        ( intro site_ board thread post backlinks (current_time m)
-        : deleteBtn_ m pwb
-        ++
-        [ files_or_embed_view
-        , div_
-            [ class_ "body" ]
-            (Body.render site_ parts)
-        ]
-        )
-    ]
-
-    where
-        files_or_embed_view :: View model Action
-        files_or_embed_view =
-          case (Post.embed post) of
-            Just _ -> embed post
-            Nothing -> files (media_root m) site_ post
-
-        site_ :: Site
-        site_ = site m
-
-        board :: Board
-        board = head $ Site.boards site_
-
-        thread :: Thread
-        thread = head $ Board.threads board
-
-
-deleteBtn_ :: Model -> PostWithBody -> [ View model Action ]
-deleteBtn_ m p
-  | admin m = [ deleteBtn (OnDeleteBtn p) ]
-  | otherwise = []
