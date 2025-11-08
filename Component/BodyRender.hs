13117e7c9fb9a4ba3d3f5f7b918a3b677e9abe68
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2025-11-08 02:23:48 -0500

    Shouldn't need to manually call htmlEscape anymore

--- Component/BodyRender.hs
+++ Component/BodyRender.hs
@@ -25,9 +25,6 @@ import Miso.Html.Property
     , target_
     , class_
     )
-#ifndef FRONT_END
-import Miso.Html.Render (htmlEncode)
-#endif
 import Miso.String (toMisoString, MisoString)
 import Data.JSString (replace)
 import System.FilePath ((</>))
@@ -57,7 +54,6 @@ render :: Site.Site -> [ PostPart ] -> [ View model a ]
 render = map . renderPostPart
 
 renderPostPart :: Site.Site -> PostPart -> View model a
-#ifdef FRONT_END
 renderPostPart _ (SimpleText txt) = text $ getRidOfCarriageReturn txt
 renderPostPart _ (PostedUrl u) =
     a_
@@ -65,15 +61,6 @@ renderPostPart _ (PostedUrl u) =
         , target_ "_blank"
         ]
         [ text u ]
-#else
-renderPostPart _ (SimpleText txt) = text $ htmlEncode $ getRidOfCarriageReturn txt
-renderPostPart _ (PostedUrl u) =
-    a_
-        [ href_ $ htmlEncode u
-        , target_ "_blank"
-        ]
-        [ text $ htmlEncode u ]
-#endif
 
 renderPostPart _ Skip = br_ []
 
