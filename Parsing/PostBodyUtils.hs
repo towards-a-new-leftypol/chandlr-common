dd58df29fa192520511fbacf16c96b5ab6771096
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2026-07-31 19:46:50 -0400

    Fix issue in collectBacklinks
    - weren't handling all cases, resulting in missing backquotes

--- Parsing/PostBodyUtils.hs
+++ Parsing/PostBodyUtils.hs
@@ -6,18 +6,23 @@ import Common.Parsing.PostPartType
 import Common.Component.Thread.Model (PostWithBody)
 import Common.Parsing.QuoteLinkParser (ParsedURL (..))
 
+import Debug.Trace (trace)
+
 collectBacklinks :: [ PostWithBody ] -> Backlinks
-collectBacklinks xs = foldr insertElement Map.empty xs
-  where
-    insertElement :: PostWithBody -> Backlinks -> Backlinks
-    insertElement (post, body) acc = foldr insertPost acc (quotedPosts body)
-      where
-        insertPost post_id = Map.insertWith (++) post_id [post]
+collectBacklinks = foldr insertElement Map.empty
+    where
+      insertElement :: PostWithBody -> Backlinks -> Backlinks
+      insertElement (post, body) acc = foldr insertPost acc (quotedPosts body)
+          where
+              insertPost post_id = Map.insertWith (++) post_id [post]
 
 
 quotedPosts :: [ PostPart ] -> [ Integer ]
 quotedPosts [] = []
-quotedPosts (Quote (Right (ParsedURL { postId = Just p })) : xs) = [p] ++ quotedPosts xs
+quotedPosts (Quote (Right (ParsedURL { postId = Just p })) : xxs)
+                                       = p               : quotedPosts xxs
+quotedPosts (Quote (Left e)     : xxs) =
+                   trace ("!!! UrlParseError !!! " ++ e) $ quotedPosts xxs
 quotedPosts ((GreenText     xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
 quotedPosts ((OrangeText    xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
 quotedPosts ((RedText       xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
@@ -26,4 +31,4 @@ quotedPosts ((Bold          xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
 quotedPosts ((Underlined    xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
 quotedPosts ((Italics       xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
 quotedPosts ((Strikethrough xs) : xxs) = quotedPosts xs ++ quotedPosts xxs
-quotedPosts _ = []
+quotedPosts ( _                 : xxs) = quotedPosts xxs
